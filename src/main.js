a6029149d6a51ec1fa3bc6886c8c19c68dbb6277
Toonzzzzzzzzzz <135047743+Toonzzzzzzzzzz@users.noreply.github.com>
2025-08-16 14:39:49 +0700

    feat: setup basic routing and home page

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './routes'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+createApp(App).use(router).use(vuetify).mount('#app')
